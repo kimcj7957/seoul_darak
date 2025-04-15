ddfa47960279a4ed4591e43e0040fb7a17ba7f4e
kimcj7957 <kimcj7957@naver.com>
2025-04-15 21:30:58 +0900

    v4

--- next.config.js
+++ next.config.js
@@ -5,7 +5,22 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://seouldarak.site' : '',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/',
+          outputPath: 'static/',
+          name: '[name].[ext]',
+        },
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
