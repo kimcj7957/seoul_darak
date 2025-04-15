1388dcdc90d92ce24e815bb33756f57e22116955
kimcj7957 <kimcj7957@naver.com>
2025-04-15 21:27:43 +0900

    v3

--- next.config.js
+++ next.config.js
@@ -1,26 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-    domains: ['seouldarak.site'],
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://seouldarak.site' : '',
   basePath: process.env.NODE_ENV === 'production' ? '' : '',
   trailingSlash: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          publicPath: process.env.NODE_ENV === 'production' ? 'https://seouldarak.site' : '',
-        },
-      },
-    });
-    return config;
-  },
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig 
